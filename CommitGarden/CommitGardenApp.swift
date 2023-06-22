85cb49872fe26eeca508271a86934fe98f8d74b9
Leeo <mizzking75@gmail.com>
2023-06-22 11:50:10 +0900

    Initial Commit

---
+++ CommitGarden/CommitGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommitGardenApp.swift
+//  CommitGarden
+//
+//  Created by hyunho lee on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct CommitGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
